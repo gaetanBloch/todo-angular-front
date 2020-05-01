a0063aa7b11c2e742f59646e8a7b0363a196f4ad
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-01 17:10:53 +0200

    Made the Header collapsable

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  collapsed = false;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
