31390dc99dc22d09fef10575dd0e0838c262ba09
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-01 13:08:33 +0200

    Display a List of TODOs in the Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ListTodosComponent } from './list-todos/list-todos.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
   {path: 'welcome', component: WelcomeComponent},
   {path: 'welcome/:username', component: WelcomeComponent},
-  {path: 'login', component: LoginComponent},
+  {path: 'todos', component: ListTodosComponent},
   {path: '**', component: NotFoundComponent}
 ];
 
--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-todos.component.css']
 })
 export class ListTodosComponent implements OnInit {
+  todos = [
+    {id: 1, description: 'Become an expert at Angular'},
+    {id: 2, description: 'Become an expert at Node.js'},
+    {id: 3, description: 'Learn Reactjs'},
+  ];
 
   constructor() { }
 
