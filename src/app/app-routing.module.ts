0b5b272d278092ca6a0e4ef0309a9b9ac1e9185b
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-01 12:05:44 +0200

    Added Not Found Error Page and the root Page redirect to the Login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'welcome', component: WelcomeComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
