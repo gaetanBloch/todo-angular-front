6825bce32ecaf82dfe81c6f92635149e7fdd1342
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-02 10:48:57 +0200

    Check if the User is authenticated in the Auth Guard

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { HardcodedAuthService } from './hardcoded-auth.service';
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
+  constructor(private authService: HardcodedAuthService) {
+  }
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
     Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.authService.isAuthenticated();
   }
 }
