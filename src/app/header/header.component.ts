ea4726ced2c542d54fcff60ec8a76c728ab73992
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-02 10:17:05 +0200

    Check th authentication state in the header to display the right controls

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HardcodedAuthService } from '../services/hardcoded-auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
+  isAuthenticated = false;
 
-  constructor() {
+  constructor(private authService: HardcodedAuthService) {
   }
 
   ngOnInit(): void {
+    this.isAuthenticated = this.authService.isAuthenticated();
   }
 
 }
