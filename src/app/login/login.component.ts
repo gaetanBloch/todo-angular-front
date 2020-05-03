4ce103730d8b5cf27f151623496da0e71df3ea01
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-03 13:43:32 +0200

    Added spinner to the login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,6 +11,7 @@ export class LoginComponent implements OnInit {
   username = 'gbloch';
   password = '';
   invalidLogin: boolean;
+  isLoading = false;
 
   constructor(private router: Router, private authService: HardcodedAuthService) {
   }
@@ -19,11 +20,14 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(): void {
+    this.isLoading = true;
     if (this.authService.login(this.username, this.password)) {
       this.invalidLogin = false;
       this.router.navigate(['welcome', this.username]);
+      this.isLoading = false;
     } else {
       this.invalidLogin = true;
+      this.isLoading = false;
     }
   }
 }
