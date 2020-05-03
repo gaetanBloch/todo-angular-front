c478b737cd4a48572e494d689e6d777a1a0a80b3
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-03 22:49:44 +0200

    Re-instate the HttpInterceptor using the SessionStorage to get the authentication token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -14,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { TodoComponent } from './todo/todo.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+import { HttpBasicAuthInterceptorService } from './shared/auth/http-basic-auth-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,7 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
     HttpClientModule
   ],
   providers: [
-    // {provide: HTTP_INTERCEPTORS, useClass: HttpBasicAuthInterceptorService, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: HttpBasicAuthInterceptorService, multi: true}
   ],
   bootstrap: [AppComponent]
 })
--- src/app/shared/auth/http-basic-auth-interceptor.service.ts
+++ src/app/shared/auth/http-basic-auth-interceptor.service.ts
@@ -3,7 +3,6 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Observable } from 'rxjs';
 
 import { BasicAuthService } from './basic-auth.service';
-import { User } from '../model/user.model';
 
 @Injectable({providedIn: 'root'})
 export class HttpBasicAuthInterceptorService implements HttpInterceptor {
@@ -12,13 +11,11 @@ export class HttpBasicAuthInterceptorService implements HttpInterceptor {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let user = this.authService.getUser();
-    if (!user) {
-      user = new User('gbloch', 'dummy');
+    const user = this.authService.getUser();
+    if (user) {
+      const newRequest = req.clone({setHeaders: {Authorization: user.authenticationToken}});
+      return next.handle(newRequest);
     }
-    // todo fix
-    const basicAuthHeaderString = 'Basic ' + window.btoa(user.username + ':' + user);
-    const newRequest = req.clone({setHeaders: {Authorization: basicAuthHeaderString}});
-    return next.handle(newRequest);
+    return next.handle(req);
   }
 }
