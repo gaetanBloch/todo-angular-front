7fb710321853d1002c6ffbb4d05d2783936af880
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-04 10:55:34 +0200

    Check if the password and its confirmation are the same on signup form

--- src/app/authenticate/signup/signup.component.ts
+++ src/app/authenticate/signup/signup.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 import { JwtAuthService } from '../../shared/auth/jwt-auth.service';
 import { Router } from '@angular/router';
 
@@ -9,8 +9,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  static readonly DIFFERENT_PASSWORDS = 'The password and its confirmation must be the same!'
   errorMessage: string;
   isLoading = false;
+  differentPasswords = false;
+  @ViewChild('passwordModel', {static: false}) password: NgModel;
+  @ViewChild('confirmPasswordModel', {static: false}) confirmPassword: NgModel;
 
   constructor(private authService: JwtAuthService, private router: Router) {
   }
@@ -18,9 +22,25 @@ export class SignupComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  checkPasswordsFirst() {
+    if (this.password.touched && this.confirmPassword.touched) {
+      this.checkPasswords();
+    }
+  }
+
+  checkPasswords() {
+    if (this.password.value !== this.confirmPassword.value) {
+      this.errorMessage = SignupComponent.DIFFERENT_PASSWORDS;
+      this.differentPasswords = true;
+    } else {
+      this.errorMessage = null;
+      this.differentPasswords = false;
+    }
+  }
+
   onSubmit(form: NgForm): void {
     this.isLoading = true;
-    this.authService.signup(form.value.username, form.value.password).subscribe(() => {
+    this.authService.signup(form.value.username, this.password.value).subscribe(() => {
       this.errorMessage = null;
       this.router.navigate(['welcome']);
       this.isLoading = false;
