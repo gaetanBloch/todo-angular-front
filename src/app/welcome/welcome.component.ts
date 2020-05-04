0d325ade706b76eefe7c3e8ac9f3730ff41e462d
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-04 13:36:15 +0200

    Re-design the welcome Page

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { WelcomeDataService } from '../shared/data/welcome-data.service';
+import { Component, OnInit } from '@angular/core';
+import { faDoorOpen, faList } from '@fortawesome/free-solid-svg-icons';
 import { JwtAuthService } from '../shared/auth/jwt-auth.service';
 
 @Component({
@@ -9,34 +7,15 @@ import { JwtAuthService } from '../shared/auth/jwt-auth.service';
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent implements OnInit, OnDestroy {
+export class WelcomeComponent implements OnInit {
   username: string;
-  welcomeMessage: string;
-  errorMessage: string;
-  private paramsSubscription: Subscription;
+  faDoorOpen = faDoorOpen;
+  faList = faList;
 
-  constructor(private route: ActivatedRoute,
-              private welcomeDataService: WelcomeDataService,
-              private authService: JwtAuthService) {
+  constructor(private authService: JwtAuthService) {
   }
 
   ngOnInit(): void {
     this.username = this.authService.getUsername();
-  }
-
-  getWelcomeMessageWithParam(): void {
-    this.welcomeDataService.getHelloWorldWithParam(this.username).subscribe(response => {
-      this.welcomeMessage = response.message;
-      this.errorMessage = null;
-    }, error => {
-      this.errorMessage = error.error;
-      this.welcomeMessage = null;
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.paramsSubscription) {
-      this.paramsSubscription.unsubscribe();
-    }
   }
 }
